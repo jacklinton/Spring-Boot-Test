ca15035696eccc33c29894db00a085604b15adf5
Jack Linton <jacklinton@mac.com>
2017-08-20 23:04:40 -0400

    Complete ArticleDAO.java.

--- src/main/java/com/testapp/dao/ArticleDAO.java
+++ src/main/java/com/testapp/dao/ArticleDAO.java
@@ -24,4 +24,22 @@ public class ArticleDAO implements com.concretepage.dao.IArticleDAO {
     public void AddArticle(Article article) {
         entityManager.persist(article);
     }
+    @Override
+    public void addArticle(Article article) {
+        Article artcl = getArticleById(article.getArticleId())
+        artcl.setTitle(article.getTitle());
+        artcl.setCategory(article.getCategory());
+        entityManager.flush();
+    }
+    @Override
+    public void deleteArticle(int articleId) {
+        entityManager.remove(getArticleById(articleId));
+    }
+    @Override
+    public boolean articleExists(String title, String category) {
+        String hql = "FROM Article as atcl WHERE atcl.title = ? and atcl.category = ?";
+        int count = entityManager.createQuery(hql).setParameter(1, title)
+                .setParameter(2, category).getResultList().size();
+        return count > 0 ? true : false;
+    }
 }
