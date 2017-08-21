9e88b65d9113c9323fe730cfb95b0fbdaade9bb1
Jack Linton <jacklinton@mac.com>
2017-08-20 23:50:55 -0400

    Complete ArticleService class and interface.

---
+++ src/main/java/com/testapp/service/IArticleService.java
@@ -0,0 +1,10 @@
+package com.testapp.service;
+import java.util.List;
+import com.testapp.entity.Article;
+public interface IArticleService {
+    List<Article> getAllArticles();
+    Article getArticleById(int articleId);
+    boolean addArticle(Article article);
+    void updateArticle(Article article);
+    void deleteArticle(int articleId);
+}
